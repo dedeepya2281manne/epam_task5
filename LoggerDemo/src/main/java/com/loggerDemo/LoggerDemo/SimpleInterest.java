d2327367e6a36eef71e7e91ab100d44457a4a4dd
dedeepya <dedeepyamanne02@gmail.com>
2020-02-20 23:19:00 +0530

    Calculating SimpleInterest & CompoundInterest with Logger

---
+++ LoggerDemo/src/main/java/com/loggerDemo/LoggerDemo/SimpleInterest.java
@@ -0,0 +1,28 @@
+package com.loggerDemo.LoggerDemo;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+
+public class SimpleInterest {
+	static Logger logger = Logger.getLogger(CalculateInterest.class);
+	float principleAmount,rate,time,simpleInterest;
+	void readValues() throws IOException{
+		Scanner sc = new Scanner(System.in);
+		logger.info("Enter Principle Amount : ");
+		principleAmount = sc.nextFloat();
+		logger.info("Enter Rate : ");
+		rate = sc.nextFloat();
+		logger.info("Enter Time Period : ");
+		time = sc.nextFloat();
+	}
+	void calculateSimpleInterest(){
+		simpleInterest = (principleAmount * rate * time)/100;
+	}
+	void displayAmount() throws IOException{
+		logger.info("Total Interest : "+simpleInterest);
+	}
+}
